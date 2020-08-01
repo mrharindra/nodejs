f3a1b253f625cf209271634ac11cfa3682e2b0cd
Harindra Chaudhary <harindra.chaudhary@seclore.com>
2020-08-01 12:01:11 +0530

    bugfix

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.use(async (req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
     res.header("Access-Control-Allow-Headers", "Authorization, Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Credentials", true);
 
     if(isAuthRequired(req.originalUrl) == false)
     {
