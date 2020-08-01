e6c9a927007b9d915b3c7a9385dab8602492017f
Harindra Chaudhary <harindra.chaudhary@seclore.com>
2020-08-01 12:56:04 +0530

    bugfix

--- app.js
+++ app.js
@@ -29,11 +29,11 @@ app.use( bodyParser.json() );
 
 app.use(async (req, res, next) => {
     
-    //res.header("Access-Control-Allow-Origin", "http://localhost:4200/");
-    //res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
-    //res.header("Access-Control-Allow-Headers", "Authorization, Origin, X-Requested-With, Content-Type, Accept");
-    //res.header("Access-Control-Allow-Credentials", true);
-    //res.header("Access-Control-Expose-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    res.header("Access-Control-Allow-Headers", "Authorization, Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Expose-Headers", "Content-Type");
 
     //console.log( req.method );
 
